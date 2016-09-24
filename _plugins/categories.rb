bb4469f7ed8e0c2d252df3ca554ad3ab768da47a
Prasanna Venkatesan <prasann@thoughtworks.com>
2016-09-24 14:17:11 +0530

    fixed category issues and formatted html

---
+++ _plugins/categories.rb
@@ -0,0 +1,33 @@
+module Jekyll
+  class CategoryPage < Page
+    def initialize(site, base, dir, category)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      process(@name)
+      read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      data['category'] = category
+
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
+      data['title'] = "#{category_title_prefix}#{category}"
+    end
+  end
+
+  class CategoryPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      if site.layouts.key? 'category_index'
+        dir = site.config['category_dir'] || 'categories'
+        all_categories = site.categories.keys.compact.uniq.reject(&:empty?)
+        all_categories.each do |category|
+          if !category.nil?
+            site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+          end
+        end
+      end
+    end
+  end
+end
--- _plugins/sass.rb
+++ _plugins/sass.rb
@@ -4,23 +4,21 @@ module Jekyll
   class SassConverter < Converter
     safe true
     priority :low
- 
-     def matches(ext)
+
+    def matches(ext)
       ext =~ /scss/i
+   end
+
+    def output_ext(_ext)
+      '.css'
     end
- 
-    def output_ext(ext)
-      ".css"
-    end
- 
+
     def convert(content)
-      begin
-        puts "Performing Sass Conversion."
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./assets/css/"], :style => :compressed)
-        engine.render
-      rescue StandardError => e
-        puts "!!! SASS Error: " + e.message
-      end
+      puts 'Performing Sass Conversion.'
+      engine = Sass::Engine.new(content, syntax: :scss, load_paths: ['./assets/css/'], style: :compressed)
+      engine.render
+    rescue StandardError => e
+      puts '!!! SASS Error: ' + e.message
     end
   end
 end
