3c0e767ca18ef34710afa79ccd3cf58779a6f5cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-09 06:51:38 +0000

    Deploy prasann/blog to github.com/prasann/blog.git:gh-pages

---
+++ README.md
@@ -0,0 +1,6 @@
+# Source code for my blog.
+
+Deployed automatically using Travis.
+
+[![Build Status](https://travis-ci.org/prasann/blog.svg?branch=master)](https://travis-ci.org/prasann/blog)
+
---
+++ assets/js/dev/prasans.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	if($('.navbar-fixed-top').length > 0){
+		var myElement = document.querySelector(".navbar-fixed-top");
+		var headroom  = new Headroom(myElement);
+		headroom.init();
+	}
+
+	$('#images').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		}
+    });
+    $('#image').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1]
+        }
+    });
+    prettyPrint();
+
+ });
